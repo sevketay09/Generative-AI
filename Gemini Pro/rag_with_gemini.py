2e9a5b0576f8bd962b37575a6b7dcda44b9c954f
Şevket Ay <63016752+sevketay09@users.noreply.github.com>
2024-02-20 18:06:18 +0300

    Delete Gemini Pro/rag_with_gemini.py

--- Gemini Pro/rag_with_gemini.py
+++
@@ -1,8 +0,0 @@
-import google.generativeai as genai
-# Used to securely store your API key
-from google.colab import userdata
-#dotenv package to load the API key
-from dotenv import load_dotenv
-from IPython.display import display
-from IPython.display import Markdown
-import textwrap
