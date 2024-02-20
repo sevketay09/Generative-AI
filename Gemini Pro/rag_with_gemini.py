9b9c6637e90a2e81cbbefaf28e3e90223afec87c
sevketay09 <sevketay09@gmail.com>
2024-02-20 18:05:22 +0300

    RAG with Gemini

---
+++ Gemini Pro/rag_with_gemini.py
@@ -0,0 +1,8 @@
+import google.generativeai as genai
+# Used to securely store your API key
+from google.colab import userdata
+#dotenv package to load the API key
+from dotenv import load_dotenv
+from IPython.display import display
+from IPython.display import Markdown
+import textwrap
